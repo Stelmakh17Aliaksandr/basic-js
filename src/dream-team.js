83d936536cbb54c17912a72f86851ddf072b343a
Stelmakh17Aliaksandr <Stelmakh17Aliaksandr@gmail.com>
2021-03-26 00:42:56 +0300

    feat: сделаные задания: коты, дрим тим; начатые: трансформация массива, какой сезон

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports =
+function createDreamTeam(members) {
+  let currentMember,
+    resultArr = [],
+      result =``;
+  if(typeof(members) != "object" || members == null) {
+    return false;
+  }
+  for(i = 0; i < members.length; i++){
+    if (typeof(members[i]) == `string`) {
+      currentMember = members[i].trimStart()
+      resultArr.push(currentMember[0].toUpperCase())
+    }
+  }
+  resultArr.sort(function(a, b){
+    if(a < b) return -1;
+    if(a > b) return 1;
+    return 0;
+})
+for (i =0; i< resultArr.length; i++) {
+  result = result+ resultArr[i]
+}
+  return result;
+
 };
+
+//console.log(createDreamTeam([`Tima`, `Gay`,`genshi`, `null`, null, true, "pohui"]))
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,39 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+let newArr =[],
+    result = [];
+  for (i = 0 ; i < arr.length; i++) {
+    switch (arr[i]) {
+      case undefined:
+        break;
+      case `--discard-next`:
+
+        arr[i+1] = undefined
+
+        break;
+      case `--discard-prev`:
+        newArr.splice(newArr.length-1)
+        break;
+      case `--double-next`:
+        if(arr[i+1] == undefined) {
+          break;
+        }
+        newArr.push(arr[i+1])
+        break;
+      case `--double-prev`:
+        if(arr[i-1] == undefined) {
+          break;
+        }
+        newArr.push(arr[i-1])
+        break;
+      default:
+
+         newArr.push(arr[i])
+         break;
+    }
+  }
+  return newArr
+
 };
+
